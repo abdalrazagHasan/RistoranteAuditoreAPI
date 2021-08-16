cfe98aefb9eebc2fff1381289d0c2f3ab648d094
Abdalrazag Hassan <abdalrazag.alshrufat@gmail.com>
2021-08-16 23:20:18 +0300

    initialize project

---
+++ src/main/java/com/abdelrazzaq/ristoranteauditore/api/RistoranteAuditoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.abdelrazzaq.ristoranteauditore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistoranteAuditoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RistoranteAuditoreApiApplication.class, args);
+    }
+
+}
